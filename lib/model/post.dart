f5dbf46d5841f61b7130a7b80f9e192f0e00d1e8
Alain <17759711186@163.com>
2019-03-12 10:44:56 +0800

    sliver new router

--- lib/model/post.dart
+++ lib/model/post.dart
@@ -27,12 +27,52 @@ final List<Post> posts = [
     imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
   ),
   Post(
-    title: 'GOD2',
+    title: 'GOD3',
     author: 'Alain',
     imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
   ),
   Post(
-    title: 'GOD2',
+    title: 'GOD4',
+    author: 'Alain',
+    imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+  ),
+  Post(
+    title: 'GOD5',
+    author: 'Alain',
+    imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+  ),
+  Post(
+    title: 'GOD6',
+    author: 'Alain',
+    imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+  ),
+  Post(
+    title: 'GOD7',
+    author: 'Alain',
+    imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+  ),
+  Post(
+    title: 'GOD8',
+    author: 'Alain',
+    imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+  ),
+  Post(
+    title: 'GOD9',
+    author: 'Alain',
+    imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+  ),
+  Post(
+    title: 'GOD10',
+    author: 'Alain',
+    imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+  ),
+  Post(
+    title: 'GOD11',
+    author: 'Alain',
+    imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+  ),
+  Post(
+    title: 'GOD12',
     author: 'Alain',
     imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
   ),
